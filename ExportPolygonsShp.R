80c2cf20d32af02655fe24ec67ea030b00012ed0
derek-corcoran-barrios <derek-corcoran-barrios@gmail.com>
2022-11-18 12:10:03 +0100

    Initial commit

---
+++ ExportPolygonsShp.R
@@ -0,0 +1,13 @@
+library(sf)
+library(terra)
+
+Poligonos <- readRDS("Poligonos_Ciudades.rds")
+
+Poligonos <- Poligonos[st_geometry_type(Poligonos) == "POLYGON",]
+
+Poligonos <- terra::vect(Poligonos)
+terra::writeVector(Poligonos, "Cities.shp")
+
+Files <- list.files(pattern = "Cities")
+
+zip("Polygons.zip", files = Files)
